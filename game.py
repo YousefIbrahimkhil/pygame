16b806e01c486e6b6784a7acc08479a37bb52882
AJ Rahim <aj.rahim@sony.com>
2023-04-13 19:01:21 -0400

    Pause added

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+paused = False
 dt = 0
 speed = 10
 size = 50
@@ -60,69 +61,69 @@ while running:
                 seconds = 0
             text = str(minutes).zfill(2) + ":" + str(seconds).zfill(2) + ":" + str(int(milliseconds)).zfill(2)
 
-            
+    if paused == False:
     
-    screen.fill("black")
-    screen.blit(font.render(text, True, (255, 255, 255)), (screen.get_width()-150, 48))
+        screen.fill("black")
+        screen.blit(font.render(text, True, (255, 255, 255)), (screen.get_width()-150, 48))
 
-    count = 0
-    row = 0
-    level = []
-    collide = False
+        count = 0
+        row = 0
+        level = []
+        collide = False
 
-    for x in pixels:
-        wallcolor = "white"
-        if x == (0,0,0,255):
+        for x in pixels:
             wallcolor = "white"
+            if x == (0,0,0,255):
+                wallcolor = "white"
+            else:
+                wallcolor = "black"
+            level_rect = rectangle(count * size, row * size, size)
+            level.append(level_rect)
+            pygame.draw.rect(screen, wallcolor, level_rect)
+            count += 1
+            if count > width - 1:
+                count = 0
+                row += 1
+
+        for index, i in enumerate(level):
+            this_collide = i.colliderect(player_pos)
+            if this_collide and pixels[index] == (0,0,0,255):
+                collide = True
+            elif this_collide and pixels[index] == (237,28,36,255):
+                print('YOU WON!')
+                paused = True
+                
+        color = (255, 0, 0) if collide else (213, 227, 16)
+        pygame.draw.rect(screen, color, player_pos)
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            direction = "up"
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            direction = "down"
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            direction = "left"
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            direction = "right"
+
+        if direction == "down":
+            player_pos.y += speed * dt
+        elif direction == "up":
+            player_pos.y -= speed * dt
+        elif direction == "left":
+            player_pos.x -= speed * dt
         else:
-            wallcolor = "black"
-        level_rect = rectangle(count * size, row * size, size)
-        level.append(level_rect)
-        pygame.draw.rect(screen, wallcolor, level_rect)
-        count += 1
-        if count > width - 1:
-            count = 0
-            row += 1
+            player_pos.x += speed * dt
 
-    for index, i in enumerate(level):
-        this_collide = i.colliderect(player_pos)
-        if this_collide and pixels[index] == (0,0,0,255):
-            collide = True
-        elif this_collide and pixels[index] == (237,28,36,255):
-            print('YOU WON!')
-            running = False
-            
-    color = (255, 0, 0) if collide else (213, 227, 16)
-    pygame.draw.rect(screen, color, player_pos)
+        speed += 1
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w] or keys[pygame.K_UP]:
-        direction = "up"
-    if keys[pygame.K_s] or keys[pygame.K_DOWN]:
-        direction = "down"
-    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-        direction = "left"
-    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-        direction = "right"
-
-    if direction == "down":
-        player_pos.y += speed * dt
-    elif direction == "up":
-        player_pos.y -= speed * dt
-    elif direction == "left":
-        player_pos.x -= speed * dt
-    else:
-        player_pos.x += speed * dt
-
-    speed += 1
-
-    if collide:
-        player_pos = rectangle(starting_x, starting_y, size)
-        speed = 10
-        direction = "right"
+        if collide:
+            player_pos = rectangle(starting_x, starting_y, size)
+            speed = 10
+            direction = "right"
 
 
-    pygame.display.flip()
-    dt = clock.tick(60) / 1000
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 pygame.quit()
